5af5c1db0b1bc3f8451504a35a6eba1addbbe23f
Nayru <nayru@teilur.com>
2022-03-23 01:23:26 -0500

    first commit

---
+++ app/src/main/java/com/example/colegio/Notas.kt
@@ -0,0 +1,11 @@
+package com.example.colegio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notas)
+    }
+}
